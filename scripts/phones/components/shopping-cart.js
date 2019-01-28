3555490797de0776f100e1148638df9c9793eb07
Sofia Ageichenko <sageichenko@gmail.com>
2019-01-28 12:10:50 +0300

    add shopping cart

--- scripts/phones/components/shopping-cart.js
+++ scripts/phones/components/shopping-cart.js
@@ -4,9 +4,30 @@ export default class ShopCart {
         this._products = {
             "4": {
                 name: "ololo",
-                qty: 3;
+                qty: 3
             }
         };
+        this._element.innerHTML = this.HTML;
+        this._initEvents();
+    }
+
+    _initEvents() {
+        this._element.addEventListener('click', (ev) => {
+            let target = ev.target;
+            if (target.classList.contains('delete-product-btn')) {
+                let product = target.closest('[data-element="cart-element"]');
+                console.log(product.getAttribute('data-phone-id'));
+                this.deleteProduct(product.getAttribute('data-phone-id'));
+            }
+        });
+    }
+
+    deleteProduct(id) {
+        this._products[id].qty--;
+        if (this._products[id].qty === 0) {
+            delete this._products[id];
+        }
+        this._render();
         this._element.innerHTML = this.HTML;
     }
 
@@ -19,13 +40,21 @@ export default class ShopCart {
                 qty: 1
             };
         }
+        this._render();
+        this._element.innerHTML = this.HTML;
     }
 
     _render () {
         this._html =`
         <ul> 
-            ${for (item )}
-        </ul>    `;
+            ${[].reduce.call(Object.keys(this._products), (acc, item) => {
+                acc+=`<li data-element="cart-element"
+                          data-phone-id="${item}">
+                          <span>${this._products[item].name}${this._products[item].qty === 1 ? '' : ` (${this._products[item].qty})`}
+                          </span><span class="delete-product-btn" data-element="delete-product-btn">✘</span></li>`;
+                return acc;
+        }, ``)}
+        </ul>`;
     }
 
     get HTML () {
